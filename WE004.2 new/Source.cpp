d57beb99d4f097d667ab019145d13b24d6379cd7
PAWAT YAMWONG <64010646@KMITL.AC.TH>
2021-08-29 20:01:16 +0700

    miss

--- WE004.2 new/Source.cpp
+++ WE004.2 new/Source.cpp
@@ -2,6 +2,7 @@
 int main() {
     int i;
     float num[100], sum = 0.0, avg;
+    printf("Enter student height:\n");
     for (i = 0; i < 10; ++i) {
         printf("student %d height: ", i + 1);
         scanf_s("%f", &num[i]);
